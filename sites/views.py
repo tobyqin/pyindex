141080ef84e29f0d8734c5f9668193f8c890f727
Toby Qin <toby.qin@live.com>
2019-05-16 23:24:42 +0800

    add site info.

--- sites/views.py
+++ sites/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+import yaml
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+def site_info(request):
+    with open('site.yml', encoding='utf8') as f:
+        info = yaml.safe_load(f)
+        return HttpResponse(str(info))
