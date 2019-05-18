ef1a5c1785dc50872ce41fe3af3ee4ff63314aca
Toby Qin <toby.qin@live.com>
2019-05-18 11:02:07 +0800

    update config.

--- manage.py
+++ manage.py
@@ -6,6 +6,8 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyindex.settings')
+    os.environ.setdefault('PyIndex.DEBUG', 'True')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
