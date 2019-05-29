90d3bc27aafa462935a10772babed4e660d43552
Toby Qin <toby.qin@live.com>
2019-05-29 22:46:47 +0800

    update settings.

--- pyindex/settings.py
+++ pyindex/settings.py
@@ -4,13 +4,13 @@ from django.conf import settings
 
 DEFAULT_PYINDEX_CFG = {
     'site': {
-        'title': 'Awesome Collection',
+        'title': 'Awesome Collections - Powered by PyIndex',
         'description': 'My collection of awesome sites.',
         'author': 'Toby Qin',
         'keywords': 'collections, sites, homepage',
         'footer': {
             'copyright': '&copy; 2018 - {}'.format(datetime.now().year),
-            'about_info': '<a href="./pages/about.html"><strong>Toby Qin</strong></a>',
+            'about_info': '<a href="./about"><strong>Toby Qin</strong></a>',
             'more_info': '</a> powered by <a href="https://github.com/tobyqin/pyindex" '
                          'target="_blank"><strong>PyIndex</strong></a>'
         }
